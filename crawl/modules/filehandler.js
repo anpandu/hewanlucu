d65698d14530d695a5fa022505c6c2dd9d14bd35
anpandu <anpandumail@gmail.com>
2015-05-10 22:42:38 +0700

    getconfig

--- crawl/modules/filehandler.js
+++ crawl/modules/filehandler.js
@@ -2,11 +2,15 @@
 var FileHandler = function () {};
 
 FileHandler.prototype.fs      = require('fs');
+FileHandler.prototype.getconfig = function (callback) {
+	FileHandler.prototype.fs.readFile('config.json', 'utf8', function (err, data) {
+		callback(data)
+	});
+}
 FileHandler.prototype.read_json = function (filepath, callback) {
 	FileHandler.prototype.fs.readFile(filepath, 'utf8', function (err, data) {
-		if (err) {
+		if (err)
 			console.log(err)
-		}
 		data = JSON.parse(data)
 		callback(data)
 	});
